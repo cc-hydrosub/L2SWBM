0d2096155450b3ff03217fb1768b5fe37ef0160a
Deanna Apps <49068950+DeannaApps@users.noreply.github.com>
2023-12-13 10:49:09 -0500

    Add files via upload

---
+++ L2SWBM_Nature2023/Code/main_ALL.r
@@ -0,0 +1,68 @@
+####################################################################################################
+####################################################################################################
+####################################################################################################
+### L2SWBM Operational Script
+### Joeseph Smith
+### University of Michigan, Cooperative Institute for Great Lakes Research
+### National Oceanic and Atmospheric Administration - Great Lakes Environmental Research Laboratory
+### Ann Arbor, MI, USA
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+library(rjags)
+
+##### SET WORKING DIRECTORY
+
+rootDir = getwd()
+#rootDir = "C:/Users/joexi/Desktop/L2SWBM_v3"
+setwd(rootDir);
+
+### GET FUNCTIONS, GLOBAL (GREAT LAKES WIDE) VARIABLES AND CONFIGURATION
+source('configEtc.r');
+
+### GET DATA
+source('data_ALL.r');
+
+source('proc_prior_ALL.r');
+
+### CREATE RESULTS DIRECTORY
+if(!file.exists(paste("results", sep=''))){
+	dir.create(paste("results", sep=''));
+}
+
+### CREATE MODEL DIRECTORY
+if(!file.exists(paste("results/", modelName, sep=''))){
+	dir.create(paste("results/", modelName, sep=''));
+}
+setwd(paste(rootDir,"/results/", modelName, sep=''))
+
+write.table(config, paste(modelName,'_config.csv', sep=''), sep=',', row.names=TRUE, col.names=c("Config", "Value"))
+
+### PREVIEW DATA
+source('../../priorPlotter.r')
+
+source('../../tsPlotter_Preview_ALL.r')
+
+### WRITE THE MODEL
+
+source('../../proc_model_write_ALL.r');
+
+### RUN THE MODEL
+
+source('../../proc_model_run_ALL_parallel.r');
+
+### SAVE DATA
+
+save.image(paste(modelName,'.RData', sep=''));
+
+### PLOTS
+### source('../../tsPlotter_ALL.r');
+source('../../tsPlotter_ALL_wSkill.r');
+
+if(checkModel){
+	source('../../statsGen.r');
+}
+
+source('../../dataGen_ALL.r');
+
